f2d9c9bd8fde6b4515e10d2fd38ac73e1d1655ad
Mathew Cohen <mathewjcohen@pm.me>
2026-06-17 20:44:37 -0500

    Fix Resend build error: lazy-init client inside sendEmail instead of module level

--- src/lib/email/sender.ts
+++ src/lib/email/sender.ts
@@ -1,10 +1,9 @@
 import { Resend } from 'resend'
 
-const resend = new Resend(process.env.RESEND_API_KEY)
-
 const FROM = 'MarkitRunner <noreply@markitrunner.com>'
 
 export async function sendEmail(to: string, subject: string, html: string) {
+  const resend = new Resend(process.env.RESEND_API_KEY)
   const { data, error } = await resend.emails.send({
     from: FROM,
     to,
